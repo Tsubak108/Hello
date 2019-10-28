a98906babd4cd84f925cb2a8921cd1a9242ca578
Sergey <s.emelyanov@sovest.com>
2019-10-28 09:56:04 +0300

    Исправил 5 задание

--- lesson1/5.py
+++ lesson1/5.py
@@ -1,11 +1,11 @@
 ravenue = int(input("Введите прибыль: "))
 costs = int(input("Введите издержки: "))
 if ravenue > costs:
-    profit = ravenue-costs
-    rent = profit/ravenue
-    print(f"Отлично.  {profit}  выручка")
+    profit = ravenue - costs
+    rent = profit / ravenue
+    print(f"Отлично.  {profit}  выручка и {rent} рентабельность")
     people = int(input("Сколько у вас работников: "))
-    print(f"{profit/people} на одного работника")
+    print(f"{profit / people:.2f} на одного работника")
 elif ravenue == costs:
     print("Не плохо")
 else:
