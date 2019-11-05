2039edc8c05023dfe3a7b252885a470ac1e25bcb
Sergey <s.emelyanov@sovest.com>
2019-11-05 15:42:46 +0300

    Поправил задание 1 и 5

--- lesson1/Задание 3.5.py
+++ lesson1/Задание 3.5.py
@@ -6,13 +6,13 @@ def my_sum():
 
         res = 0
         for el in range(len(number)):
-             if number[el] == 'q' or number[el] == 'Q':
+            if number[el] == 'q' or number[el] == 'Q':
                 ex = True
-        break
-    else:
-            res = res + int(number[el])
-            sum_res = sum_res + res
-            print(f'Current sum is {sum_res}')
-            print(f'Your final sum is {sum_res}')
+                break
+            else:
+                res = res + int(number[el])
+        sum_res = sum_res + res
+    print(f'Current sum is {sum_res}')
+    print(f'Your final sum is {sum_res}')
 
 my_sum()
